f9831361cbc9f12f83e825e64ea9e5fe230dc8ba
Blacepos <jraustin999@gmail.com>
2023-01-30 03:14:15 -0500

    Removed some dead code

--- util/helpers.js
+++ util/helpers.js
@@ -5,11 +5,6 @@ async function getSentiment(str) {
     // Creates a client
     const client = new language.LanguageServiceClient();
 
-    /**
-     * TODO(developer): Uncomment the following line to run this code.
-     */
-    // const text = 'Your text to analyze, e.g. Hello, world!';
-
     // Prepares a document, representing the provided text
     const document = {
     content: str,
@@ -20,16 +15,8 @@ async function getSentiment(str) {
     const [result] = await client.analyzeSentiment({document});
 
     const sentiment = result.documentSentiment;
-    // console.log('Document sentiment:');
-    // console.log(`  Score: ${sentiment.score}`);
-    // console.log(`  Magnitude: ${sentiment.magnitude}`);
 
     const sentences = result.sentences;
-    // sentences.forEach(sentence => {
-    // console.log(`Sentence: ${sentence.text.content}`);
-    // console.log(`  Score: ${sentence.sentiment.score}`);
-    // console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
-    // });
 
     return sentiment.score
 }
